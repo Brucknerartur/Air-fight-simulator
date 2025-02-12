08b40d041098571d35f3e8b4cb28f72f77da8b63
Bruckner Artur <bruckner.artur.benedek@students.jedlik.eu>
2025-02-12 09:23:43 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Air_fight_simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
